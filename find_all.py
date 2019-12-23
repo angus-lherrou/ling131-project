1fda5299e272ca30359d0cda659799cef49cd730
Dominique O'Donnell <dom115461@users.noreply.github.com>
2019-12-23 00:30:42 -0500

    Updated find_all.py
    Removed unused module imports, added comments

--- find_all.py
+++ find_all.py
@@ -1,21 +1,23 @@
+'''
+find_all.py
+
+Script to run functions from module to get locations
+in a given episode text. 
+Retrieve max 5 episodes per show.
+'''
+
 import os
 import re
 import json
-import gzip
-import time
 import finalfindlocs as fl
 
 
-
+#modify these to match corpus in and out paths
 INPATH = "compressed_ling131-project"
 OUTPATH = "located_data"
 
 
-
-def get_text(inpath, outpath):
-    fl.get_data(inpath, outpath)
-    return 
-
+#traverse the directory and retrieve episode files
 def traverse_dir():
     counter = 0
     for folder in os.listdir(INPATH):
@@ -32,23 +34,28 @@ def traverse_dir():
                 episode_count = 0
                 for filename in os.listdir(infolder):
                     if episode_count < 5:
-                        infile = os.path.join(infolder, filename)
-                        if filename.endswith('.gz'):
-                            continue
-                        t0 = time.time()                   
-                        x = fl.get_data(infile, savedir)
-                        if x == 0:
-                            return
-                        t1 = time.time()
-                        counter += 1
-                        print(counter)
-                        with open('read.txt', 'a') as f:
-                            f.write(os.path.join(infolder, filename) + ":" + str(t1-t0) + ":\n")
+                        get_file(infolder, filename)
                         episode_count += 1
+         
     return
-                   
+             
+
+#Process file with fl.get_data method
+def get_file(infolder, filename):
+    infile = os.path.join(infolder, filename)
+    if filename.endswith('.gz'):
+        continue
+    t0 = time.time()
+    #store return val (1=success, 0=failure) to track Geonames Exceed errot                   
+    x = fl.get_data(infile, savedir)
+    if x == 0:
+        return
+    with open('read.txt', 'a') as f:
+        f.write(os.path.join(infolder, filename) + ":" + str(t1-t0) + ":\n")      
+    return
+
+
 if not os.path.exists(OUTPATH):
     os.mkdir(OUTPATH)
-
 
 traverse_dir()
